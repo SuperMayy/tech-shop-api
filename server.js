45916db8100d6d6033ef2f83e45d89eae6778c86
SuperMayy <mayowashowumi@outlook.com>
2020-01-20 21:14:20 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('./body-parser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var db = mongoose.connect('mongodb://localhost/tech-shop');
 
 app.use(bodyParser.json());
-app,use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.listen(3000, function() {
     console.log("Tech Shop API running on Port 3000!...")
